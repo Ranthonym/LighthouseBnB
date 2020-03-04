a7fe7380635d671f2dc00f3653cf44a24c21716a
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-03-04 22:41:03 +0000

    add queries

---
+++ 1_queries/all_my_reservations.sql
@@ -0,0 +1,19 @@
+SELECT properties.*, reservations.*, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
+WHERE reservations.guest_id = 1
+AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
+
+
+
+-- SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+--  FROM property_reviews
+--  JOIN properties ON property_id = properties.id
+--  JOIN reservations ON reservation_id = reservations.property_id
+--  WHERE reservations.end_date < now()::date AND reservations.guest_id = 1
+--  GROUP BY reservations.id, properties.id
+--  LIMIT 10;
