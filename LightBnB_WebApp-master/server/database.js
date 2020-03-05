04bdc04bfbb0a11d0a1a10a2f776b1a3b6c4cb34
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-03-05 19:16:16 +0000

    add user reservations query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -65,7 +65,17 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id 
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    LIMIT $2;
+  `, [guest_id, limit])
+  .then(res => res.rows);
+
 }
 exports.getAllReservations = getAllReservations;
 
