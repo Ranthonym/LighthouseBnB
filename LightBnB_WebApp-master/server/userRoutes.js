dd191cee0dc67389b7a0a687e9263b0c179f7fe9
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-03-05 18:36:15 +0000

    add user register/login query functions

---
+++ LightBnB_WebApp-master/server/userRoutes.js
@@ -0,0 +1,81 @@
+const bcrypt = require('bcrypt');
+
+module.exports = function(router, database) {
+
+  // Create a new user
+  router.post('/', (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 12);
+    database.addUser(user)
+    .then(user => {
+      if (!user) {
+        res.send({error: "error"});
+        return;
+      }
+      req.session.userId = user.id;
+      res.send("🤗");
+    })
+    .catch(e => res.send(e));
+  });
+
+  /**
+   * Check if a user exists with a given username and password
+   * @param {String} email
+   * @param {String} password encrypted
+   */
+  const login =  function(email, password) {
+    return database.getUserWithEmail(email)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        return user;
+      }
+      return null;
+    })
+    .catch(error => console.error(error));
+  }
+  exports.login = login;
+
+  router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+    login(email, password)
+      .then(user => {
+        console.log({user})
+        if (!user) {
+          res.status(401).json({message: "Unauthorized"});
+          return;
+        }
+        req.session.userId = user.id;
+        res.send({user: {name: user.name, email: user.email, id: user.id}});
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ error });
+      });
+  });
+  
+  router.post('/logout', (req, res) => {
+    req.session.userId = null;
+    res.send({});
+  });
+
+  router.get("/me", (req, res) => {
+    const userId = req.session.userId;
+    if (!userId) {
+      res.send({message: "not logged in"});
+      return;
+    }
+
+    database.getUserWithId(userId)
+      .then(user => {
+        if (!user) {
+          res.send({error: "no user with that id"});
+          return;
+        }
+    
+        res.send({user: {name: user.name, email: user.email, id: userId}});
+      })
+      .catch(e => res.send(e));
+  });
+
+  return router;
+}
